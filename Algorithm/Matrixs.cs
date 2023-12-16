fb716a1ca4cc0c122061cd4f7a286c71418b3aaa
mihuwis <mihuwis@gmail.com>
2023-12-16 12:56:47 +0100

    Add

---
+++ Algorithm/Matrixs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Algorithm
+{
+    internal class Matrixs
+    {
+        public int determinantOfSquare2Matrix(int[,] matrix)
+        {
+            if(matrix.Length != 4)
+            {
+                throw new ArgumentException("Array to big");
+            }
+
+            return (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        }
+    }
+}
--- Algorithm/Sorting.cs
+++ Algorithm/Sorting.cs
@@ -25,9 +25,78 @@ namespace Sandbox.Algorithm
                 }
             }
             DateTime endTime = DateTime.Now;
-            Console.WriteLine(endTime.Subtract(startTime).TotalMilliseconds);
+            Console.WriteLine("Bubble sort time : " + endTime.Subtract(startTime).TotalMilliseconds);
 
             return arrayToSort;
+        }
+
+        public int[] insertionSort(int[] arrayToSort)
+        {
+            DateTime startTime = DateTime.Now;
+            for (int i = 1; i < arrayToSort.Length ; i ++)
+            {
+                int keyValue = arrayToSort[i];
+                bool isSorted = false;
+                for(int j = i - 1; j >= 0 && !isSorted;)
+                {
+                    if (keyValue < arrayToSort[j])
+                    {
+                        arrayToSort[j+1] = arrayToSort[j];
+                        j--;
+                        arrayToSort[j + 1] = keyValue;
+                    }
+                    else
+                    {
+                        isSorted = true;
+                    }
+                }
+            }
+
+            DateTime endTime = DateTime.Now;
+            Console.WriteLine("insert sort time : " + endTime.Subtract(startTime).TotalMilliseconds);
+
+
+            return arrayToSort;
+        }
+
+        public int[] quickSort(int[] arrayToSort, int begin, int end)
+        {
+
+            if (begin < end)
+            {
+                int pivotIndex = pivot(arrayToSort, begin, end);
+
+                quickSort(arrayToSort, begin, pivotIndex-1);
+                quickSort(arrayToSort, pivotIndex+1, end);
+            }
+
+
+
+            return arrayToSort;
+
+        }
+
+        private int pivot(int[] arr, int begin, int end)
+        {
+            int pivot = arr[end];
+            int i = begin - 1;
+
+            for (int j = begin; j < end; j ++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i++;
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            int swapTemp = arr[i + 1];
+            arr[i + 1] = arr[end];
+            arr[end] = swapTemp;
+            return i + 1;
         }
     }
 }
